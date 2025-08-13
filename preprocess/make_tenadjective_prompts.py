6c8e5fe8682105dff21874a37a9abc53607f4029
Eric Xu <exu@boslogin06.rc.fas.harvard.edu>
2025-08-12 23:39:24 -0400

    Made updates

---
+++ preprocess/make_tenadjective_prompts.py
@@ -0,0 +1,8 @@
+import json
+
+"""
+Creates a JSON file of prompts asking an LLM to produce a list of ten adjectives that accurately describe the
+smell of a particular food sample in the VCF dataset.
+
+"""
+
